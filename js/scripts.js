b3fe6a47f869b6512444c17f9a23495e97d88623
Erika Mogollon <fany200025@hotmail.com>
2022-02-01 18:54:30 -0800

    it will add new files

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,71 @@
 // Business Logic
 
-function wordCounter(text) {
+// function wordCounter(text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function(word) {
+//     wordCount++;
+//   });
+//   return wordCount;
+// }
+
+// Business Logic
+
+// function wordCounter(text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function(element) {
+//     if (!Number(element)) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
+
+// function numberOfOccurrencesInText(word, text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   const wordArray = text.split(" ");
+//   let wordCount = 0;
+//   wordArray.forEach(function(element) {
+//     if (word === element) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
+
+// function numberOfOccurrencesInText(word, text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   const wordArray = text.split(" ");
+//   let wordCount = 0;
+//   wordArray.forEach(function(element) {
+//     if (word.toLowerCase() === element.toLowerCase()) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
+
+function numberOfOccurrencesInText(word, text) {
   if (text.trim().length === 0) {
     return 0;
   }
-  let wordCount = 0;
   const wordArray = text.split(" ");
-  wordArray.forEach(function(word) {
-    wordCount++;
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
   });
   return wordCount;
 }
